60192b4b4683d6337026819f94b88c01943b36df
phillyagyu <107158459+phillyagyu@users.noreply.github.com>
2022-06-19 02:07:32 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ function onScroll() {
   activateMenuAtCurrentSection(home)
   activateMenuAtCurrentSection(services)
   activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(testimonials)
   activateMenuAtCurrentSection(contact)
 }
 
@@ -21,7 +22,7 @@ function showNavOnScroll() {
 
 function activateMenuAtCurrentSection(section) {
   // innherHeight is a number  that represents the size of the viewport
-  const targetLine = scrollY + innerHeight/2
+  const targetLine = scrollY + innerHeight / 2
 
   const sectionTop = section.offsetTop
   const sectionHeight = section.offsetHeight
@@ -32,7 +33,8 @@ function activateMenuAtCurrentSection(section) {
 
   const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
 
-  const sectionBoundaries = sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
 
   const sectionId = section.getAttribute('id')
   const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
@@ -59,6 +61,44 @@ function closeMenu() {
   document.body.classList.remove('menu-expanded')
 }
 
+// Lib swiperJs
+
+// Swiper of #testimonials refeering as 's1' instead of 'swiper'
+const swiper1 = new Swiper('.s1', {
+  slidesPerView: 1,
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  mousewheel: true,
+  keyboard: true,
+
+  breakpoints: {
+    1024: {
+      slidesPerView: 2,
+    },
+  }
+})
+
+// Swiper of #brands refeering as 's2' instead of 'swiper'
+const swiper2 = new Swiper('.s2', {
+  slidesPerView: 2,
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  mousewheel: true,
+  keyboard: true,
+
+  breakpoints: {
+    1024: {
+      slidesPerView: 3,
+    },
+  }
+})
+
+
+// Lib scrollReveal
 ScrollReveal({
   origin: 'top',
   distance: '30px',
@@ -72,4 +112,10 @@ ScrollReveal({
   #services .card,
   #about,
   #about header,
-  #about .content`)
+  #about .content,
+  #testimonials,
+  #testimonials header,
+  #testimonials .content,
+  #brands,
+  #brands header,
+  #brands .content`)
